efeb7f24395d13fe022f2c435e08aa70822fd034
Timur15V <volkovtimur042@gmail.com>
2024-04-25 23:17:06 +0300

    updating

--- main/views.py
+++ main/views.py
@@ -2,12 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import context
 
+from goods.models import Categories
+
 
 # Create your views here.
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         "title": "MelodyStore - Главная",
         "content": "Магазин музыкальных инструментов MelodyStore",
+        "categories": categories,
     }
 
     return render(request, "main/index.html", context)
