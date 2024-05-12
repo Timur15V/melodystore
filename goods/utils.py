f3231fbc673fa389c645f0316ae122eccdb0866d
Timur15V <volkovtimur042@gmail.com>
2024-05-12 16:05:31 +0300

    Postgresql connected and full search

--- goods/utils.py
+++ goods/utils.py
@@ -1,5 +1,7 @@
 import keyword
+from re import search
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from goods.models import Products
 
@@ -9,13 +11,23 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    keywords = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("name", "description", "vendorCode")
 
-    q_objects = Q()
+    query = SearchQuery(query)
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-        q_objects |= Q(vendorCode__icontains=token)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
-    return Products.objects.filter(q_objects)
+    # return Products.objects.annotate(
+    #     search=SearchVector("name", "description", "vendorCode")
+    # ).filter(search=query)
+
+    # keywords = [word for word in query.split() if len(word) > 2]
+
+    # q_objects = Q()
+
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #     q_objects |= Q(vendorCode__icontains=token)
+
+    # return Products.objects.filter(q_objects)
--- melodystore/settings.py
+++ melodystore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     "debug_toolbar",
     "main",
     "goods",
@@ -81,8 +82,12 @@ WSGI_APPLICATION = "melodystore.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "melodystore_db",
+        "USER": "timur",
+        "PASSWORD": "498496",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
