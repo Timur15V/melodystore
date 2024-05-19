806c26c5041ae14be499363044d713028b64fe1e
Timur15V <volkovtimur042@gmail.com>
2024-05-19 23:40:07 +0300

    add to cart ajax

--- static/deps/js/vue/apps.js
+++ static/deps/js/vue/apps.js
@@ -1,15 +1,40 @@
-var app = new Vue({
-    delimiters: ['[[', ']]'],
-    el: '#app',
-    data: {
-        message: "Hello, Vue!",
-    },
-    mounted() {
-        console.log(this.message)
-    },
-    methods: {
-        clickCart() {
-            console.log(this.message);
-        }
-    }
-})
+
+// var app = new Vue({
+//     delimiters: ['[[', ']]'],
+//     el: '#app',
+//     data: {
+//         count: 0,
+//     },
+//     mounted() {
+//         console.log(this.message)
+//     },
+//     methods: {
+//         click_minus() {
+//             this.count--;
+//         },
+//         click_plus() {
+//             this.count++;
+//         },
+//         async cart_add(product_id) {
+//             console.log(product_id)
+//             let url = document.getElementById('cart_add').href
+//             let csrf_token = grabToken()
+//             // let csrf_token = document.getElementsByName('csrfmiddlewaretoken')[0].defaultValue
+//             console.log(url)
+//             console.log(csrf_token)
+
+//             let formdata = new FormData()
+
+//             formdata.append("product_id", product_id)
+//             formdata.append("csrfmiddlewaretoken", csrf_token)
+
+//             axios({
+//                 method: 'post',
+//                 url: url,
+//                 data: formdata
+//             }).then(function (response) {
+//                 console.log(response)
+//             });
+//         }
+//     }
+// })
