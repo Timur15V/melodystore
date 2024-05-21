0ad0ca25cc7a3b088de2c26571e658c86e7e7f7f
Timur15V <volkovtimur042@gmail.com>
2024-05-21 22:59:25 +0300

    sessions anonym user

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from traitlets import Instance
 
+from carts.models import Cart
 from users.forms import ProfileForm, UserLoginFrom, UserRegistrationForm
 
 
@@ -18,9 +19,15 @@ def login(request):
             username = request.POST["username"]
             password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 if request.POST.get("next", None):
                     return HttpResponseRedirect(request.POST.get("next"))
@@ -43,8 +50,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+
             messages.success(
                 request,
                 f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт",
